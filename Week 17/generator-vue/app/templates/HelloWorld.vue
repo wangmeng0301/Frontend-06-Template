38b8fbe1e1e8d9e3a5b137c0b1fe8d0f482461d8
wangmeng-ls@bestpay.com.cn <wangmeng-ls@bestpay.com.cn>
2021-02-24 21:50:02 +0800

    第十七周

---
+++ Week 17/generator-vue/app/templates/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <p>{{ gretting }} World</p>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      gretting: 'Hello',
+    }
+  },
+}
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
